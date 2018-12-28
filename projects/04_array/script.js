fd6185617782d52ee54ccb9f6dd4f4c1158c3927
Yang Dai <yang@Yangs-MacBook-Pro.local>
2018-12-27 23:45:47 -0500

    starter file

---
+++ projects/04_array/script.js
@@ -0,0 +1,61 @@
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+//
+// Some data we can work with
+
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first, 'Galieo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first, 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first, 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Hanna', last: 'Hammarstrom', year: 1829, passed: 1909 },
+];
+
+const people = [
+    'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick',
+    'Beecher, Henry', 'Beethoven Ludwig', 'Begin, Menachem',
+    'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert',
+    'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter',
+    'Benn, Tony', 'Bennington, chester', 'Benson, Leana',
+    'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar',
+    'Berio, Luciano', 'Berle, Milton', 'Berline, Irving', 'Berne, Eric',
+    'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell',
+    'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph',
+    'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
+    'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blaire, Tony',
+    'Blake, William',
+];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+
+// Array.prototype.map()
+// 2. Give us an array of the invenstor's first and last names
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+
+// Array.prototype.reduce()
+// 4. How may years did all the inventors live?
+
+// 5. Sort the inventors by years lived
+
+// 6. create a list of Boulevards in Pairs that contain 'de' anywhere in the same
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = [
+    'car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van',
+    'bike', 'walk', 'car', 'van', 'car', 'truck',
+]
