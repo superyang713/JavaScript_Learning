e102bcdab22be9272d460da6cf84ebf4f247417d
Yang Dai <yang@Yangs-MacBook-Pro.local>
2019-01-06 20:53:11 -0500

    refactor

--- ch07/main.js
+++ ch07/main.js
@@ -15,25 +15,12 @@ const roads = [
   "Shop-Town Hall",
 ];
 
-function buildGraph(edges) {
-  let graph = Object.create(null);
-  function addEdge(from, to) {
-    if (graph[from] == null) {
-      graph[from] = [to];
-    } else {
-      graph[from].push(to);
-    }
-  }
-
-  for (let [from, to] of edges.map(r => r.split("-"))) {
-    addEdge(from, to);
-    addEdge(to, from);
-  }
-
-  return graph;
-}
-
-const roadGraph = buildGraph(roads);
+const mailRoute = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+];
 
 class VillageState {
   constructor(place, parcels) {
@@ -52,6 +39,37 @@ class VillageState {
       return new VillageState(destination, parcels);
     }
   }
+}
+
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({place, address});
+  }
+  return new VillageState("Post Office", parcels);
+}
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+
+  return graph;
 }
 
 function runRobot(state, robot, memory) {
@@ -75,28 +93,6 @@ function randomPick(array) {
 function randomRobot(state) {
   return {direction: randomPick(roadGraph[state.place])};
 }
-
-VillageState.random = function(parcelCount = 5) {
-  let parcels = [];
-  for (let i = 0; i < parcelCount; i++) {
-    let address = randomPick(Object.keys(roadGraph));
-    let place;
-    do {
-      place = randomPick(Object.keys(roadGraph));
-    } while (place == address);
-    parcels.push({place, address});
-  }
-  return new VillageState("Post Office", parcels);
-}
-
-runRobot(VillageState.random(), randomRobot);
-
-const mailRoute = [
-  "Alice's House", "Cabin", "Alice's House", "Bob's House",
-  "Town Hall", "Daria's House", "Ernie's House",
-  "Grete's House", "Shop", "Grete's House", "Farm",
-  "Marketplace", "Post Office"
-];
 
 function routeRobot(state, memory) {
   if (memory.length == 0) {
@@ -129,3 +125,6 @@ function goalOrientedRobot({place, parcels}, route) {
   }
   return {direction: route[0], memory: route.slice(1)};
 }
+
+const roadGraph = buildGraph(roads);
+runRobot(VillageState.random(), randomRobot);
