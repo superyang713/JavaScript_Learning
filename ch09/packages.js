1767c098904185be7073607aae132c3b0cc72ad9
Yang Dai <yang@Yangs-MacBook-Pro.local>
2019-01-06 23:49:26 -0500

    module

---
+++ ch09/packages.js
@@ -0,0 +1,15 @@
+import ordinal from "ordinal";
+import {days, months} from "date-names";
+
+export function formatDate(date, format) {
+  return format.replace(/YYYY|M(MMM)?|Do?|dddd/g, tag => {
+    if (tag == "YYYY") return date.getFullYear();
+    if (tag == "M") return date.getMonth();
+    if (tag == "MMMM") return months[date.getMonth()];
+    if (tag == "D") return date.getDate();
+    if (tag == "Do") return ordinal(date.getDate());
+    if (tag == "dddd") return days[date.getDay()];
+  });
+};
+
+console.log(formatDate(new Date(2019, 8, 13), "dddd the Do"));
