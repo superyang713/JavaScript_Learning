83f40498fa5d34209752de4fc807f6ccaccefce8
Yang Dai <yangdai713@aim.com>
2019-01-07 14:12:54 -0500

    update project 09

---
+++ eloquent/class_notation.js
@@ -0,0 +1,13 @@
+class Rabbit {
+    hello(type) {
+	this.type = type;
+    }
+    speak(line) {
+	console.log(`The ${this.type} rabbit says '${line}'`);
+    }
+}
+
+let killerRabbit = new Rabbit("killer");
+let blackRabbit = new Rabbit("black");
+
+killerRabbit.speak("hello");
